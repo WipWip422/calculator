e5099661f3099b895047b96605d52214b15e248c
WipWip <ahnafartist@gmail.com>
2025-09-25 12:04:47 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 let result = document.getElementById('result');
+let fieldOne = document.getElementById('fieldOne');
+let fieldTwo = document.getElementById('fieldTwo');
 
-// get numbers from input field
 function getNumbers() {
-    let fieldOne = Number(document.getElementById('fieldOne').value);
-    let fieldTow = Number(document.getElementById('fieldTwo').value);
+    let fieldOne = document.getElementById('fieldOne').value;
+    let fieldTwo = document.getElementById('fieldTwo').value;
+  
+    if(fieldOne === '' || fieldTwo === '') {
+        result.textContent = 'Field is empty';
+        return null;
+    }
 
-    return [fieldOne, fieldTow];
+    return [Number(fieldOne), Number(fieldTwo)];
 }
 
-// refresh the input fields and result
 function refresh() {
     result.textContent = 'Result';
     document.getElementById('fieldOne').value = null;
@@ -16,31 +21,41 @@ function refresh() {
 }
 document.getElementById('refresh').addEventListener('click', refresh);
 
-// operation
-function addition() {
-    let [a, b] = getNumbers();
-    result.textContent = a + b;
-}
-function subtraction() {
-    let [a, b] = getNumbers();
-    result.textContent = a - b;
-}
-function multiplication() {
-    let [a, b] = getNumbers();
-    result.textContent = a * b;
-}
-function division() {
-    let [a, b] = getNumbers();
-    result.textContent = a / b;
-}
-function reminder() {
-    let [a, b] = getNumbers();
-    result.textContent = a % b;
+function calculate(operations) {
+    let numbers = getNumbers(); 
+    if(!numbers) return;
+
+    let [a, b] = numbers;
+    let output;
+
+    switch(operations) {
+        case 'add':
+            output = a + b;
+            break;
+        case 'subtract':
+            output = a - b;
+            break;
+        case 'multiply':
+            output = a * b;
+            break;
+        case 'divide':
+            output = b === 0 ? "Can't divide with 0" : parseFloat((a / b).toFixed(9));
+            break;
+        case 'remainder':
+            output = b === 0 ? "Can't divide with 0" : parseFloat((a % b).toFixed(9));
+            break;
+        case 'power':
+            output = Math.pow(a, b);
+            break;
+        default:
+            output = 'Unknown Operation';
+    }
+    result.textContent = output;
 }
 
-// events
-document.getElementById('add').addEventListener('click', addition);
-document.getElementById('subtract').addEventListener('click', subtraction);
-document.getElementById('multiply').addEventListener('click', multiplication);
-document.getElementById('divide').addEventListener('click', division);
-document.getElementById('reminder').addEventListener('click', reminder);
+document.getElementById('add').addEventListener('click', () => calculate('add'));
+document.getElementById('subtract').addEventListener('click', () => calculate('subtract'));
+document.getElementById('multiply').addEventListener('click', () => calculate('multiply'));
+document.getElementById('divide').addEventListener('click', () => calculate('divide'));
+document.getElementById('remainder').addEventListener('click', () => calculate('remainder'));
+document.getElementById('power').addEventListener('click', () => calculate('power'));
